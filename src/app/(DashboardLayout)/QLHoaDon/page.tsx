3afa80bc4246a9339efa740939f83113ed136a02
TechNguyen <ndt13102003@gmail.com>
2025-05-17 22:08:04 +0700

    1

--- src/app/(DashboardLayout)/QLHoaDon/page.tsx
+++ src/app/(DashboardLayout)/QLHoaDon/page.tsx
@@ -3,33 +3,110 @@
 import Flex from "@/components/shared-components/Flex";
 import AutoBreadcrumb from "@/components/util-compenents/Breadcrumb";
 import useQLHoaDon from "@/hooks/useQLHoaDon";
-import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import classes from "./QLHoaDon.module.scss";
+import { tableOrderDataType } from "@/interface/Order/Order";
+import { CloseOutlined, PlusCircleOutlined, SearchOutlined } from "@ant-design/icons";
+import { Button, Card, Table } from "antd";
+import { TableProps } from "antd/lib";
+import classes from "./page.module.css";
 const QLHoaDon = () =>
 {
-    const {handleGetData, lstOrders, isPannelSearch} = useQLHoaDon()
+    const {handleGetData, lstOrders, loading, isPannelSearch, setIsPannelSearch} = useQLHoaDon()
 
 
+    const tableColumns: TableProps<tableOrderDataType>["columns"] = [
+        {
+            title: "Mã hóa đơn",
+            dataIndex: "id",
+            key: "id",
+            render: (recrod: tableOrderDataType) => <span>{recrod.name}</span>,
+        },
+        {
+            title: "Tên khách hàng",
+            dataIndex: "customerName",
+            key: "customerName",
+        },
+        {
+            title: "Ngày tạo",
+            dataIndex: "createdDate",
+            key: "createdDate",
+            render: (recrod: tableOrderDataType) => <span>{recrod.name}</span>,
+        },
+        {
+            title: "Trạng thái",
+            dataIndex: "status",
+            key: "status",
+        },
+        {
+            title: "Tổng tiền",
+            dataIndex: "totalPrice",
+            key: "totalPrice",
+        }
+    ]
 
     return (
+        <>
+            <Flex
+                alignItems="center"
+                justifyContent="space-between"
+                className={classes.mgButton10}
+            >
+                <AutoBreadcrumb />
+                <div>   
+                    <Button
+                        type="primary"
+                        className={classes.mgButton10}
+                        onClick={() => setIsPannelSearch(!isPannelSearch)}
+                        icon={isPannelSearch ? <CloseOutlined /> : <SearchOutlined />}
+                    >
+                        {isPannelSearch ? "Ẩn tìm kiếm" : "Tìm kiếm"}
+                    </Button>
+                </div>
 
-        <Flex
-            alignItems="center"
-            justifyContent="space-between"
-            className={classes.mgButton10}
-        >
-            <AutoBreadcrumb />
-            <div>   
+
                 <Button
-                    type="primary"
-                    className={classes.mgButton10}
-                    icon={isPannelSearch ? <CloseOutlined /> : <SearchOutlined />}
-                >
-                    {/* {isPannelSearch ? ""} */}
-                </Button>
-            </div>
-        </Flex>
+                onClick={() => {
+                //   handleShowModal();
+                }}
+                type="primary"
+                icon={<PlusCircleOutlined />}
+                size="small"
+            >
+                Thêm mới
+            </Button>
+            </Flex>
+          <Card style={{ padding: "0px" }} className={classes.customCardShadow}>
+        <div className="table-responsive">
+          <Table
+            columns={tableColumns}
+            bordered
+            dataSource={lstOrders}
+            rowKey="id"
+            scroll={{ x: "max-content" }}
+            pagination={false}
+            loading={loading}
+          />
+        </div>
+        {/* <Pagination
+          className="mt-2"
+          total={dataPage?.totalCount}
+          showTotal={(total, range) =>
+            `${range[0]}-${range[1]} trong ${total} dữ liệu`
+          }
+          pageSize={pageSize}
+          defaultCurrent={1}
+          onChange={(e) => {
+            setPageIndex(e);
+          }}
+          onShowSizeChange={(current, pageSize) => {
+            setPageIndex(current);
+            setPageSize(pageSize);
+          }}
+          size="small"
+          align="end"
+        /> */}
+      </Card>
+        
+        </>
 
     )
 
